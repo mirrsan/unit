6d1680447934b7c8d28b3ecd78e1b1f1df3fb4e7
mirrsan <46742702+mirrsan@users.noreply.github.com>
2019-03-21 18:20:51 +0100

    Add files via upload

---
+++ ZadatakNeki.test/RepositoryTest/KancelarijaRepositoryTest.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autofac.Extras.Moq;
+using Moq;
+using Xunit;
+using ZadatakNeki.Models;
+using ZadatakNeki.Repository;
+
+namespace ZadatakNeki.test.RepositoryTest
+{
+
+    public class KancelarijaRepositoryTest
+    {
+        [Fact]
+        public void DajSveEntitete_KadRadi()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<ToDoContext>().Setup(k => k.Kancelarije.ToList()).Returns(lazneKa());
+
+                var klasa = mock.Create<KancelarijaRepository>();
+
+                var ocekujes = lazneKa();
+                var dobijas = klasa.DajSveEntitete();
+
+                Assert.True(dobijas != null);
+                Assert.Equal(ocekujes.Count, dobijas.Count);
+                for (int i = 0; i < lazneKa().Count; i++)
+                {
+                    Assert.Equal(ocekujes[i], dobijas[i]);
+                }
+            }
+        }
+
+        public List<Kancelarija> lazneKa()
+        {
+            List<Kancelarija> kancelarije = new List<Kancelarija>()
+            {
+                new Kancelarija()
+                {
+                    Opis = "nestoVako"
+                },
+                new Kancelarija()
+                {
+                    Opis = "marketing"
+                },
+                new Kancelarija()
+                {
+                    Opis = "dizajn"
+                },
+                new Kancelarija()
+                {
+                    Opis = "sala za sastanke"
+                }
+            };
+            return kancelarije;
+        }
+
+        [Theory]
+        [InlineData(6)]
+        public void EntitetPoId_KadRadi(long id)
+        {
+            Kancelarija ocekujes = lazneKa()[0];
+
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<ToDoContext>().Setup(w => w.Kancelarije.Find(It.IsAny<long>())).Returns(ocekujes);
+
+                var dobijas = mock.Create<KancelarijaRepository>().EntitetPoId(id);
+
+                Assert.Equal(ocekujes, dobijas);
+            }
+        }
+
+        //[Fact]
+        //public void DodajEntitet_KadRadi()
+        //{
+        //    Kancelarija ocekujes = lazneKa()[0];
+
+        //    using (var mock = AutoMock.GetLoose())
+        //    {
+        //        mock.Mock<ToDoContext>().Setup(v => v.Kancelarije.Add(ocekujes));
+
+                
+        //    }
+        //}
+
+        //[Fact]
+        //public void Izmeni_WhenWork()
+        //{
+        //}
+
+        [Theory]
+        [InlineData("kantina")]
+        public void PretragaPoNazivu_WhenRadi(string ime)
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                Kancelarija kancelarija = new Kancelarija() {Opis = "kantina"};
+
+                mock.Mock<ToDoContext>()
+                    .Setup(c => c.Kancelarije.Where(k => k.Opis == It.IsAny<string>()).ToString())
+                    .Returns(kancelarija.ToString);
+
+                var dobijas = mock.Create<KancelarijaRepository>().PretragaPoNazivu(ime);
+
+                Assert.Equal(kancelarija, dobijas);
+            }
+        }
+    }
+}
---
+++ ZadatakNeki.test/RepositoryTest/OsobaRepositoryTest.cs
@@ -0,0 +1,58 @@
+﻿using Autofac.Extras.Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
+using Remotion.Linq.Parsing.ExpressionVisitors.Transformation.PredefinedTransformations;
+using Xunit;
+using ZadatakNeki.Controllers;
+using ZadatakNeki.Models;
+using ZadatakNeki.Repository;
+using ZadatakNeki.Repositorys.IRepository;
+
+namespace ZadatakNeki.test.RepositoryTest
+{
+    public class OsobaRepositoryTest
+    {
+        [Fact]
+        public void DajSveEntitete_KadRadi()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<ToDoContext>().Setup(g => g.Osobe.ToList()).Returns(NekeOsobe());
+
+                var ocekujes = NekeOsobe();
+                var dobijas = mock.Create<OsobaRepository>().DajSveEntitete();
+
+                Assert.Equal(ocekujes.Count, dobijas.Count);
+            }
+        }
+
+        public List<Osoba> NekeOsobe()
+        {
+            List<Osoba> osobe = new List<Osoba>()
+            {
+                new Osoba()
+                {
+                    Ime = "mirsan",
+                    Prezime = "kajovic",
+                    Kancelarija = new Kancelarija() {Opis = "kuca"}
+                },
+                new Osoba()
+                {
+                    Ime = "maida",
+                    Prezime = "rondic",
+                    Kancelarija = new Kancelarija() {Opis = "sala za sastanke"}
+                },
+                new Osoba()
+                {
+                    Ime = "semsa",
+                    Prezime = "semsovic",
+                    Kancelarija = new Kancelarija() {Opis = "kantina"}
+                }
+            };
+            return osobe;
+        }
+    }
+}
